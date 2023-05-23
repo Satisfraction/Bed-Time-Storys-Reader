15b00b90da299c1d5ebbfd981f4997bfa1ddb45a
Satisfraction <122011782+Satisfraction@users.noreply.github.com>
2023-05-24 01:55:45 +0200

    Update BedStorysGUI.py
    add new function for volume control

--- BedStorysGUI.py
+++ BedStorysGUI.py
@@ -7,57 +7,55 @@ class BedStorysGUI(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
         self.initUI()
-        self.media_player = None  # Fügt das Attribut media_player hinzu und initialisiert es mit None
+        self.media_player = None
 
     def initUI(self):
-        # Ändert die Hintergrundfarbe des Fensters
         self.setStyleSheet("background-color: #EFEFEF;")
-        
-        # Ändert die Schriftart und -größe des Fenstertitels
         font = QtGui.QFont()
         font.setPointSize(16)
         font.setBold(True)
         self.setFont(font)
         self.setWindowTitle("Mat´s Geschichten Vorleser")
-        
-        # Erstellen des ListWidget
+
         self.listWidget = QtWidgets.QListWidget(self)
         self.listWidget.setGeometry(50, 50, 200, 400)
         self.listWidget.setStyleSheet("background-color: #D3D3D3; border: 2px solid black;")
         self.listWidget.setFont(QtGui.QFont("Arial", 14))
         self.listWidget.itemClicked.connect(self.play_text_from_title)
 
-        # Füge die Titel Elemente der Liste hinzu
         titles = self.get_titles_from_database()
         for title in titles:
             item = QtWidgets.QListWidgetItem(title)
             item.setBackground(QtGui.QColor("#F5F5F5"))
             self.listWidget.addItem(item)
 
-        # Erstellenung des QTextEdit-Feld
         self.textEdit = QtWidgets.QTextEdit(self)
         self.textEdit.setGeometry(270, 50, 400, 400)
         self.textEdit.setStyleSheet("background-color: #D3D3D3; border: 2px solid black;")
         self.textEdit.setFont(QtGui.QFont("Arial", 14))
         self.textEdit.setReadOnly(True)
 
-        # Erstellenung der Buttons
         self.playButton = QtWidgets.QPushButton("Play", self)
         self.playButton.setGeometry(50, 470, 80, 40)
         self.playButton.setStyleSheet("background-color: green; color: white; border-radius: 5px;")
         self.playButton.clicked.connect(self.play_current_text)
 
-        self.pauseButton = QtWidgets.QPushButton('Pause', self)
+        self.pauseButton = QtWidgets.QPushButton("Pause", self)
         self.pauseButton.setGeometry(150, 470, 80, 40)
         self.pauseButton.setStyleSheet("background-color: red; color: white; border-radius: 5px;")
         self.pauseButton.clicked.connect(self.pause_audio)
 
-        self.resumeButton = QtWidgets.QPushButton('Resume', self)
+        self.resumeButton = QtWidgets.QPushButton("Resume", self)
         self.resumeButton.setGeometry(250, 470, 80, 40)
         self.resumeButton.setStyleSheet("background-color: blue; color: white; border-radius: 5px;")
         self.resumeButton.clicked.connect(self.resume_audio)
 
-        # Größe des Fensters festlegen
+        self.volumeSlider = QtWidgets.QSlider(QtCore.Qt.Horizontal, self)
+        self.volumeSlider.setGeometry(350, 470, 200, 40)
+        self.volumeSlider.setRange(0, 100)
+        self.volumeSlider.setValue(50)
+        self.volumeSlider.valueChanged.connect(self.set_volume)
+
         self.setGeometry(500, 300, 700, 550)
         self.show()
 
@@ -91,12 +89,12 @@ class BedStorysGUI(QtWidgets.QMainWindow):
         if self.media_player is not None:
             self.media_player.play()
 
-# Hauptprogramm
-if __name__ == '__main__':
-    # Erstelle eine Qt-Anwendung
+    def set_volume(self, value):
+        if self.media_player is not None:
+            self.media_player.setVolume(value)
+
+if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
-    # Erstelle ein BedStorysGUI-Objekt
     ex = BedStorysGUI()
-    # Starte die Anwendung und beende sie, wenn sie beendet wird
     sys.exit(app.exec_())
-                                                                                                                                                                                                               
+
