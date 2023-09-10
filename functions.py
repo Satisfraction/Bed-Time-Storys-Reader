95c8ccc6c021d77bfbbb6fe485ab3f55096670c9
Satisfraction <122011782+Satisfraction@users.noreply.github.com>
2023-09-11 00:01:11 +0200

    Update functions.py
    overall optimizing

--- functions.py
+++ functions.py
@@ -6,37 +6,30 @@ from gtts import gTTS
 import tempfile
 import threading
 
+# Create a global database connection object
+conn = sqlite3.connect("bed_storys.db")
+
+
 def text_to_speech(text):
     tts = gTTS(text=text, lang='de')
     tts.save("output.mp3")
     os.system("start output.mp3")
 
-def get_story_titles_from_database():
-    # Öffne die Datenbank
-    conn = sqlite3.connect("bed_storys.db")
-    c = conn.cursor()
 
-    # Führe eine Abfrage aus, um alle Titel der Geschichten zu erhalten
+def get_story_titles_from_database():
+    c = conn.cursor()
     c.execute("SELECT name FROM Storys ORDER BY id ASC")
     rows = c.fetchall()
-
-    # Schließe die Datenbank
-    conn.close()
-
-    # Extrahiere die Titel aus den Zeilen und gib sie als Liste zurück
     titles = [row[0] for row in rows]
     return titles
 
 
 def get_text_from_database(title):
-    conn = sqlite3.connect("bed_storys.db")
     c = conn.cursor()
-
     c.execute("SELECT text FROM Storys WHERE name=?", (title,))
     result = c.fetchone()
-    conn.close()
-
     return result[0] if result else None
+
 
 def play_text_with_tts(text):
     tts = gTTS(text=text, lang='de')
@@ -47,21 +40,24 @@ def play_text_with_tts(text):
 
     return file_name
 
+
 def play_story(title, text_edit):
     text = get_text_from_database(title)
     file_name = play_text_with_tts(text)
-    play_audio(file_name)
+    threading.Thread(target=play_audio, args=(file_name,)).start()
     display_text(text, text_edit)
+
 
 def play_audio(file_name):
     os.system(f'start {file_name}')
+
 
 def display_text(text, text_edit):
     cursor = text_edit.textCursor()
     cursor.movePosition(QTextCursor.End)
     cursor.insertText(text)
 
+
 def load_story_titles_to_list(list_widget):
     titles = get_story_titles_from_database()
     list_widget.addItems(titles)
-
