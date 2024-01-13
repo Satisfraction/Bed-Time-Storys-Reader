5e629bdcab39004b17144df22c1cd098811b4929
Satisfraction <122011782+Satisfraction@users.noreply.github.com>
2024-01-13 13:09:39 +0100

    Optimization update and new stories
    some optimization updates
    add 10 more new stories

--- BedStorys.py
+++ BedStorys.py
@@ -8,17 +8,34 @@ from functions import get_text_from_database, play_text_with_tts, get_story_titl
 
 
 class BedStoriesGUI(QMainWindow):
+    font = QFont('Arial', 16)
+    button_styles = {
+        'play_button': {
+            'background-color': 'green',
+            'color': 'white',
+            'border-radius': '5px'
+        },
+        'pause_button': {
+            'background-color': 'red',
+            'color': 'white',
+            'border-radius': '5px'
+        },
+        'resume_button': {
+            'background-color': 'blue',
+            'color': 'white',
+            'border-radius': '5px'
+        }
+    }
+
     def __init__(self):
         super().__init__()
         self.media_player = None
+        self.titles = self.get_titles_from_database()
         self.initUI()
 
     def initUI(self):
         self.setStyleSheet('background-color: #EFEFEF;')
-        font = QFont()
-        font.setPointSize(16)
-        font.setBold(True)
-        self.setFont(font)
+        self.setFont(self.font)
         self.setWindowTitle('Mat´s Geschichten Vorleser')
 
         central_widget = QWidget()
@@ -28,10 +45,10 @@ class BedStoriesGUI(QMainWindow):
         self.list_widget = QListWidget()
         self.list_widget.setStyleSheet(
             'background-color: #D3D3D3; border: 2px solid black;')
-        self.list_widget.setFont(QFont('Arial', 18))  # Increase font size
+        self.list_widget.setFont(QFont('Arial', 18))
         self.list_widget.itemClicked.connect(self.play_text_from_title)
-        titles = self.get_titles_from_database()
-        for title in titles:
+
+        for title in self.titles:
             item = QListWidgetItem(title)
             item.setBackground(QtGui.QColor('#F5F5F5'))
             self.list_widget.addItem(item)
@@ -39,29 +56,25 @@ class BedStoriesGUI(QMainWindow):
         self.text_edit = QTextEdit()
         self.text_edit.setStyleSheet(
             'background-color: #D3D3D3; border: 2px solid black;')
-        # Use a playful font and increase font size
         self.text_edit.setFont(QFont('Comic Sans MS', 20))
         self.text_edit.setReadOnly(True)
 
         button_layout = QHBoxLayout()
         self.play_button = QPushButton('Play')
-        self.play_button.setStyleSheet(
-            'background-color: green; color: white; border-radius: 5px;')
-        self.play_button.setFont(QFont('Arial', 16))
+        self.play_button.setStyleSheet(self.get_button_style('play_button'))
+        self.play_button.setFont(self.font)
         self.play_button.setMinimumHeight(40)
         self.play_button.clicked.connect(self.play_current_text)
 
         self.pause_button = QPushButton('Pause')
-        self.pause_button.setStyleSheet(
-            'background-color: red; color: white; border-radius: 5px;')
-        self.pause_button.setFont(QFont('Arial', 16))
+        self.pause_button.setStyleSheet(self.get_button_style('pause_button'))
+        self.pause_button.setFont(self.font)
         self.pause_button.setMinimumHeight(40)
         self.pause_button.clicked.connect(self.pause_audio)
 
         self.resume_button = QPushButton('Resume')
-        self.resume_button.setStyleSheet(
-            'background-color: blue; color: white; border-radius: 5px;')
-        self.resume_button.setFont(QFont('Arial', 16))
+        self.resume_button.setStyleSheet(self.get_button_style('resume_button'))
+        self.resume_button.setFont(self.font)
         self.resume_button.setMinimumHeight(40)
         self.resume_button.clicked.connect(self.resume_audio)
 
@@ -76,21 +89,16 @@ class BedStoriesGUI(QMainWindow):
 
         left_layout = QtWidgets.QVBoxLayout()
         left_layout.addWidget(self.list_widget)
-        # Add spacing between list_widget and volume_slider
         left_layout.addStretch(1)
         left_layout.addWidget(self.volume_slider)
-        # Increase the height of the list widget
         self.list_widget.setMinimumHeight(500)
 
         right_layout = QtWidgets.QVBoxLayout()
         right_layout.addWidget(self.text_edit)
-        # Add spacing between text_edit and button_layout
         right_layout.addStretch(1)
         right_layout.addLayout(button_layout)
-        # Increase the height of the text edit widget
         self.text_edit.setMinimumHeight(500)
 
-        # Specify row and column positions
         self.layout.addLayout(left_layout, 0, 0)
         self.layout.addLayout(right_layout, 0, 1)
 
@@ -131,6 +139,14 @@ class BedStoriesGUI(QMainWindow):
     def set_volume(self, value):
         if self.media_player is not None:
             self.media_player.setVolume(value)
+
+    def get_button_style(self, button_name):
+        style = self.button_styles.get(button_name, {})
+        return 'background-color: {}; color: {}; border-radius: {};'.format(
+            style.get('background-color', ''),
+            style.get('color', ''),
+            style.get('border-radius', '')
+        )
 
 
 if __name__ == '__main__':
